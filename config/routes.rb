8495971733bdc7ced6f783af6aa970249952ddbe
tojio <firebirdv.toji.fv5cl7@gmail.com>
2020-03-08 11:32:51 +0900

    出品商品一覧

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/items' => 'items#index'
+  # post '/items' => 'items'
+
+  resources :items
+
+
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
